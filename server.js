40df4650abc0993cc51c81e7644be980de8a2560
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2023-10-18 14:21:20 -0700

    server should now be able to display a condensed list of games i've been credited on as QA

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors")
 
 // JSON file imports
 const projects_SE = require("./datafiles/projects_SE.json")
+const qaGameCreds_sm = require("./datafiles/gamecreds_sm_qa.json")
 
 // PORT number declaration
 const PORT = process.env.PORT || 4000
@@ -22,4 +23,9 @@ app.listen(PORT, () => {
 // Home route
 app.get("/", (req, res) => {
     res.send("Nothing to See Here")
+})
+
+// Game credits condensed List
+app.get("/gcqac", (req, res) => {
+    res.json(qaGameCreds_sm)
 })

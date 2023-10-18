0564c33e6b968e1bfaaa1ee82224f96997a4a533
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2023-10-18 14:32:29 -0700

    Router Import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import Header from './components/header';
 // CSS import
 import './App.css';
 
+// Router Import 
+import {Route, Routes } from "react-router-dom"
+
+// Main App
 function App() {
   return (
     <div className="App">
---
+++ frontend/src/pages/qaexperience.js
@@ -0,0 +1,4 @@
+const QAexp = () => {
+
+}
+export default QAexp

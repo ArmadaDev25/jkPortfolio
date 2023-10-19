10b617d6e4e8890ff811bef8a8fc3f2c7d926603
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2023-10-18 21:31:47 -0700

    created the projects page and finished the routes in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/header';
 import AboutMe from './pages/aboutme';
 import Home from './pages/home';
 import QAexp from './pages/qaexperience';
+import Projects from './pages/projects';
 
 // CSS import
 import './App.css';
@@ -19,7 +20,9 @@ function App() {
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
-
+        <Route exact path="/aqexperience" element={<QAexp />} />
+        <Route exact path="/aboutme" element={<AboutMe />} />
+        <Route exact path="/projects" element={<Projects />} />
       </Routes>
 
       
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -3,6 +3,7 @@ const Home = () => {
     return (
         <div>
             <h3>Welcome to my Portfolio</h3>
+            <p>Click here to see Github repo for this site!!</p>
             
         </div>
     )
